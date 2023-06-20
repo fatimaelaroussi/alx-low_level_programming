5336c6606dd811c34819ccb402cb81b0689db843
fatimaelaroussi <fatimaelaroussi1@gmail.com>
2023-06-19 19:13:20 -0700

    print alphabet

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+  *main - entry value
+  *Return: always 0
+  */
+int main(void)
+{
+	int i;
+
+	for (i = 97; i < 123; i++)
+	{
+		if (i == 101 || i == 113)
+			continue;
+		else
+			putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
