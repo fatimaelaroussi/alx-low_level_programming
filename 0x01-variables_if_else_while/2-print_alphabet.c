a10710df44259fec024de0bf1423d912211769fe
fatimaelaroussi <fatimaelaroussi1@gmail.com>
2023-06-19 18:39:42 -0700

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+  *main - entry point
+  *Return: always 0
+  */
+int main(void)
+{
+	int i;
+	for (i=97; i<123; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
