7441eb0ad8cb39e302d0cebc4902ec3fd0eed57f
fatimaelaroussi <fatimaelaroussi1@gmail.com>
2023-06-19 19:54:55 -0700

    print comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *main - entry value
+ *Return: always 0
+ */
+int main(void)
+{
+	int i;
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
