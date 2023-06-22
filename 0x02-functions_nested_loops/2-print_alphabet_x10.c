52c8be801c5afa2db3f8934769f48e68d7cd904c
fatimaelaroussi <fatimaelaroussi1@gmail.com>
2023-06-21 19:02:21 -0700

    lower case 10 time

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,18 +1,18 @@
 #include "main.h"
 /**
-  *void print_alphabet_x10(void) - print alphabet in lower case
+  *print_alphabet_x10 - print alphabet in lower case 10 time
   *Return: always 0
   */
-void print_alphabet_x10(void);
+void print_alphabet_x10(void)
 {
 	int i, j;
 
-	for (j = 0; i < 10; j++)
+	for (j = 0; j < 10; j++)
 	{
-	for (i = 97; i <= 122; i++)
-	{
-		_putchar(i);
+		for (i = 97; i <= 122; i++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
 	}
-	}
-	_putchar('\n');
 }
