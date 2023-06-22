c7dcdd1a24668e227e15cc61881e5fd631d0fe88
fatimaelaroussi <fatimaelaroussi1@gmail.com>
2023-06-21 20:00:27 -0700

    absolue

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *_abs - check the abs value
+ *@n: number
+ *Return: 1 . 0 for zero
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (-1 * n);
+	}
+}
