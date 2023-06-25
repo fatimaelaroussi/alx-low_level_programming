8abfc6e979aee47f9304730d9ed192d80b879e35
fatimaelaroussi <fatimaelaroussi1@gmail.com>
2023-06-25 12:39:21 -0700

    square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+  *print_square - print square
+  * Return: square
+  *@n: number of square
+  */
+void print_square(int n)
+{
+	int i;
+	int j;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < n; j++)
+			{
+				_putchar('#');
+			}
+		_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
+
