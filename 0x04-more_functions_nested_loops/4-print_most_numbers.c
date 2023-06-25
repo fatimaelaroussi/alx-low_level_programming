44047b2e7317ed590166ceb1f064ee2c2345c5a2
fatimaelaroussi <fatimaelaroussi1@gmail.com>
2023-06-25 09:44:30 -0700

    print most numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,13 +9,13 @@ void print_most_numbers(void)
 
 	for (i = 48; i <= 57; i++)
 	{
-		if (i != 50 || i != 52)
+		if (i = 50 || i = 52)
 		{
-			_putchar(i);
+			continue;
 		}
 		else
 		{
-			continue;
+			_putchar(i);
 		}
 	}
 	_putchar('\n');
