cac1a921e5a3220fab2c831a04307be4dadff733
fatimaelaroussi <fatimaelaroussi1@gmail.com>
2023-06-25 12:23:52 -0700

    diagonl

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,16 +7,25 @@
 void print_diagonal(int n)
 {
 	int i;
+	int j;
 
 	if (n > 0)
 	{
 		for (i = 0; i < n; i++)
 		{
-			_putchar(' ');
-			_putchar(92);
-			_putchar('\n');
+			for (j = 0; j < n; j++)
+			{
+				if (i == j)
+				{
+					_putchar(92);
+				}
+				else if (j <= i)
+				{
+					_putchar(' ');
+				}
 		}
 		_putchar('\n');
+		}
 	}
 	else
 	{
