ae0346389daffc3e7c8fe957af6a477968d0d427
fatimaelaroussi <fatimaelaroussi1@gmail.com>
2023-06-25 16:10:50 -0700

    prime factor

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -7,21 +7,17 @@
 int main(void)
 {
 	long int i;
-	long int max;
 	long int n;
 
-	max = 2;
 	n = 612852475143;
 	while (n % 2 == 0)
 	{
-		max = 2;
 		n = n / 2;
 	}
 	for (i = 3; i <= sqrt(n); i = i + 2)
 	{
 		while (n % i == 0)
 		{
-			max = i;
 			n = n / i;
 		}
 	}
