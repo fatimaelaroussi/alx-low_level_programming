2e577b79296f942616b2a8ca71ba19b5b9d0e62e
fatimaelaroussi <fatimaelaroussi1@gmail.com>
2023-06-27 18:11:43 -0700

    revers string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+/**
+  *print_rev - returns a string in revers
+  *Return: nothing
+  *@s: string
+ */
+void print_rev(char *s)
+{
+	int i;
+
+	for (i = strlen(s); i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	 _putchar('\n');
+}
